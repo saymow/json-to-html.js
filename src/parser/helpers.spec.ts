9144a5713deb9157b975728c9d3f926a565def42
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-02-08 17:25:53 -0300

    test: ensure isArray helper returns correctly

--- src/parser/helpers.spec.ts
+++ src/parser/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { isPrimitive } from './helpers'
+import { isPrimitive, isArray } from './helpers'
 import faker from 'faker'
 
 describe('Helpers', () => {
@@ -40,7 +40,49 @@ describe('Helpers', () => {
     })
 
     it('should return false for Function', () => {
-      expect(isPrimitive(() => {})).toBeFalsy()
+      expect(isPrimitive(() => { })).toBeFalsy()
+    })
+  })
+
+  describe('isArray', () => {
+    it('should return true for Array', () => {
+      expect(isArray([faker.random.word(), faker.random.number()])).toBeTruthy()
+    })
+
+    it('should return false for string', () => {
+      expect(isArray(faker.random.word())).toBeFalsy()
+    })
+
+    it('should return false for number', () => {
+      expect(isArray(faker.random.number())).toBeFalsy()
+    })
+
+    it('should return false for null', () => {
+      expect(isArray(null)).toBeFalsy()
+    })
+
+    it('should return false for undefined', () => {
+      expect(isArray(undefined)).toBeFalsy()
+    })
+
+    it('should return false for Date', () => {
+      expect(isArray(new Date())).toBeFalsy()
+    })
+
+    it('should return false for Symbol', () => {
+      expect(isArray(Symbol(faker.random.word()))).toBeFalsy()
+    })
+
+    it('should return false for Object', () => {
+      expect(isArray({
+        [faker.random.word()]: faker.random.number(),
+        [faker.random.word()]: faker.random.number(),
+        [faker.random.word()]: faker.random.number()
+      })).toBeFalsy()
+    })
+
+    it('should return false for Function', () => {
+      expect(isArray(() => { })).toBeFalsy()
     })
   })
 })
