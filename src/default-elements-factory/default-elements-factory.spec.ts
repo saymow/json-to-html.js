0a37151d6006395757577839647d5a99d372df0f
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-02-08 16:05:37 -0300

    test: ensure DefaultElementsFactory.createSectionHeader() returns a valid sectionHeader

--- src/default-elements-factory/default-elements-factory.spec.ts
+++ src/default-elements-factory/default-elements-factory.spec.ts
@@ -8,20 +8,34 @@ describe('DefaultElementsFactory', () => {
   describe("createSection()", () => {
     it('Should create a valid section element', () => {
       const sut = makeSut()
-      const element = sut.createSection()
+      const section = sut.createSection()
 
-      expect(element.tagName.toLowerCase()).toEqual('section')
-      expect(Array.from(element.classList)).toEqual(['section-container'])
+      expect(section.tagName.toLowerCase()).toEqual('section')
+      expect(Array.from(section.classList)).toEqual(['section-container'])
     })
   })
   
   describe("createContainer()", () => {
     it('Should create a valid container element', () => {
       const sut = makeSut()
-      const element = sut.createContainer()
+      const container = sut.createContainer()
 
-      expect(element.tagName.toLowerCase()).toEqual('article')
-      expect(Array.from(element.classList)).toEqual(['base-container'])
+      expect(container.tagName.toLowerCase()).toEqual('article')
+      expect(Array.from(container.classList)).toEqual(['base-container'])
+    })
+  })
+  
+  describe("createSectionHeader()", () => {
+    it('Should create a valid sectionHeader element', () => {
+      const sut = makeSut()
+      const title = "some-title"
+      const header = sut.createSectionHeader(title)
+      const headerTitle = header.querySelector('h2')
+
+      expect(header.tagName.toLowerCase()).toEqual('header')
+      expect(Array.from(header.classList)).toEqual(['section-header'])
+      expect(headerTitle).toBeDefined()
+      expect(headerTitle!.textContent).toBe(title)
     })
   })
 })
