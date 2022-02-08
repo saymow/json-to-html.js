ab05e68c6fee08ae5cf94d243740746d73067022
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2022-02-08 16:22:56 -0300

    test: ensure DefaultElementsFactory.createValue() returns a valid value

--- src/default-elements-factory/default-elements-factory.spec.ts
+++ src/default-elements-factory/default-elements-factory.spec.ts
@@ -14,7 +14,7 @@ describe('DefaultElementsFactory', () => {
       expect(Array.from(section.classList)).toEqual(['section-container'])
     })
   })
-  
+
   describe("createContainer()", () => {
     it('Should create a valid container element', () => {
       const sut = makeSut()
@@ -24,7 +24,7 @@ describe('DefaultElementsFactory', () => {
       expect(Array.from(container.classList)).toEqual(['base-container'])
     })
   })
-  
+
   describe("createSectionHeader()", () => {
     it('Should create a valid sectionHeader element', () => {
       const sut = makeSut()
@@ -45,19 +45,29 @@ describe('DefaultElementsFactory', () => {
       const key = "some-key"
       const value = "some-value"
       const field = sut.createField(key, value)
-      const fieldKey = field.querySelector('.key') 
-      const fieldValue = field.querySelector('.value') 
+      const fieldKey = field.querySelector('.key')
+      const fieldValue = field.querySelector('.value')
 
       expect(field.tagName.toLowerCase()).toEqual('div')
       expect(Array.from(field.classList)).toEqual(['field-container'])
-      
+
       expect(fieldKey).toBeDefined()
       expect(fieldKey!.tagName.toLowerCase()).toEqual('p')
       expect(fieldKey!.textContent).toEqual(`${key}:`)
-      
+
       expect(fieldValue).toBeDefined()
       expect(fieldValue!.tagName.toLowerCase()).toEqual('p')
       expect(fieldValue!.textContent).toEqual(value)
+    })
+  })
+
+  describe("createValue()", () => {
+    it('Should create a valid value element', () => {
+      const sut = makeSut()
+      const value = sut.createValue("some-value")
+
+      expect(value!.tagName.toLowerCase()).toEqual('p')
+      expect(value!.textContent).toEqual("some-value")
     })
   })
 })
